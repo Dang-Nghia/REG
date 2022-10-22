5c8cd2bb4d9f3b58339e309ed8818e85e4b71922
Dang-Nghia <113897565+Dang-Nghia@users.noreply.github.com>
2022-10-22 20:05:47 +0700

    Update REG.py

--- REG.py
+++ REG.py
@@ -3,3 +3,10 @@ import re
 str = "dau ai biet lan gap luc do ngoi ke ben nhau la lan cuoi cung"
 
 partern = "[^trang]"
+
+match = re.search(partern, str)
+
+if match:
+  print("da tim kiem thanh cong")
+else:
+  print("tim kiem da that bai")
